0a85987357aa4123121ce66442bdf87f59b905e5
macktheknife13 <163837585+macktheknife13@users.noreply.github.com>
2025-07-31 14:36:38 +1000

    Update streamlit_app.py
    rearrange nutrition contents from bottom into existing framework.
    fruit_chosen section

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Custom Your Smoothie! :cup_with_straw: ")
@@ -30,6 +30,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #t.write(ingredients_string)
     my_insert_stmt = f"""
@@ -49,8 +51,4 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
         
-#new section to display smoothiefroot nutrition information 
-import requests
-smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
