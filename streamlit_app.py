28ba488b1afa72d5f8792b269847e83ba95ff5c3
macktheknife13 <163837585+macktheknife13@users.noreply.github.com>
2025-07-31 14:43:44 +1000

    Update streamlit_app.py
    fix syntax error for prev change

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruitchosen + 'Nurtrition Information')
+        st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon"+ fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
