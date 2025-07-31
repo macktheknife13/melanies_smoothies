da7e429a7d1a75ff357ee094654541f25c91c8cb
macktheknife13 <163837585+macktheknife13@users.noreply.github.com>
2025-07-31 14:29:35 +1000

    Update streamlit_app.py
    switched out the URL due to 504 error

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,5 +51,5 @@ if ingredients_list:
         
 #new section to display smoothiefroot nutrition information 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
 st.text(smoothiefroot_response.json())
