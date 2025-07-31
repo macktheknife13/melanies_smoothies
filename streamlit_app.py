a830040415baec5552083e1501929921284fce1f
macktheknife13 <163837585+macktheknife13@users.noreply.github.com>
2025-07-31 14:32:16 +1000

    Update streamlit_app.py
    syntax error of true into True

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,4 @@ if ingredients_list:
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container-width=true)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container-width=True)
